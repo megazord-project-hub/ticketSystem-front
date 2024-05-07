bd95e27199640402e60ab0295d0aa2390109ffdb
Carlos Krefer <carloskrefer@hotmail.com>
2024-05-07 10:09:24 -0300

    Adiciona validação no formulário de login

--- src/app/molecules/login-molecule/login-molecule.component.ts
+++ src/app/molecules/login-molecule/login-molecule.component.ts
@@ -1,10 +1,51 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login-molecule',
   templateUrl: './login-molecule.component.html',
   styleUrls: ['./login-molecule.component.scss']
 })
-export class LoginMoleculeComponent {
+export class LoginMoleculeComponent implements OnInit {
+  
+  meuForm!: FormGroup; 
+  mensagemErroSenha: string;
+  mensagemErroLogin: string;
+
+  constructor() {
+    this.mensagemErroSenha = '';
+    this.mensagemErroLogin = '';
+  }
+
+  ngOnInit(): void {
+    this.meuForm = new FormGroup({
+      'login': new FormControl(null, [Validators.required]),
+      'senha': new FormControl(null, [Validators.required, Validators.minLength(3)])
+    });
+  }
+
+  onSubmit(): void {
+    console.log(this.meuForm);    
+  }
+
+  atualizarMensagemErroLogin(): void {
+    const erros = this.meuForm.get('login')?.errors;
+    if (erros?.['required']) {
+      this.mensagemErroLogin = 'O login é obrigatório.';
+      console.log(this.mensagemErroSenha);
+    }
+  }
+
+  atualizarMensagemErroSenha(): void {
+    const erros = this.meuForm.get('senha')?.errors;
+    if (erros?.['required']) {
+      this.mensagemErroSenha = 'A senha é obrigatória.';
+      console.log(this.mensagemErroSenha);
+    } else if (erros?.['minlength']) {
+      this.mensagemErroSenha = 'Exigido pelo menos 3 caracteres.';
+    }
+  }
+
+  
 
 }
