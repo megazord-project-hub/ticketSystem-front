be217fe1122728075e364f195ce8d2834f1829ed
Carlos Krefer <carloskrefer@hotmail.com>
2024-05-22 10:18:19 -0300

    Remove espaços em branco e import não utilizado

--- src/app/epics/login/components/pages/auth-page-login/auth-page-login.component.ts
+++ src/app/epics/login/components/pages/auth-page-login/auth-page-login.component.ts
@@ -1,11 +1,8 @@
 import { Component } from '@angular/core';
-import { BoxOrganismLoginComponent } from '../../organisms/box-organism-login/box-organism-login.component';
 
 @Component({
   selector: 'app-auth-page-login',
   templateUrl: './auth-page-login.component.html',
   styleUrls: ['./auth-page-login.component.scss']
 })
-export class AuthPageLoginComponent {
-
-}
+export class AuthPageLoginComponent {}
