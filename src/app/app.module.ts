70f91354aef76532229d58f05fd264087e293b7d
Carlos Krefer <carloskrefer@hotmail.com>
2024-06-18 11:35:38 -0300

    Migração versão v16 para v18

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,27 +4,20 @@ import { AppRoutingModule } from './app-routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { StoreModule } from '@ngrx/store';
 import { EffectsModule } from '@ngrx/effects';
-import { HttpClientModule } from '@angular/common/http';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { CoreModule } from './core/core.module';
 import { CoreBootstrapComponent } from './core/components/core-bootstrap/core-bootstrap.component';
 
-@NgModule({
-  declarations: [],
-  imports: [
-    HttpClientModule,
-    BrowserModule,
-    BrowserAnimationsModule,
-    // AppRoutingModule should always be the last module imported into
-    // this array, relative to other modules that define routes. Otherwise,
-    // the wildcard path (**) will ignore their route paths.
-    AppRoutingModule,
-    CoreModule,
-    EffectsModule.forRoot([]), 
-    StoreModule.forRoot({}), 
-    StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: !isDevMode() })  
-  ],
-  providers: [],
-  bootstrap: [CoreBootstrapComponent]
-})
+@NgModule({ declarations: [],
+    bootstrap: [CoreBootstrapComponent], imports: [BrowserModule,
+        BrowserAnimationsModule,
+        // AppRoutingModule should always be the last module imported into
+        // this array, relative to other modules that define routes. Otherwise,
+        // the wildcard path (**) will ignore their route paths.
+        AppRoutingModule,
+        CoreModule,
+        EffectsModule.forRoot([]),
+        StoreModule.forRoot({}),
+        StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: !isDevMode() })], providers: [provideHttpClient(withInterceptorsFromDi())] })
 export class AppModule { }
